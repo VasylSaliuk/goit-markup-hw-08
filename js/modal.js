dc81386e0caab7d842299058c27e2be960f2d193
Vasyl Saliuk <vace1986@gmail.com>
2020-09-12 10:38:20 +0300

    finish adaptive for main page

--- js/modal.js
+++ js/modal.js
@@ -10,5 +10,11 @@
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
+  }
+  
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
     }
   })();
+
